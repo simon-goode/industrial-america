24b3f6d9c8f4762fd62cd0ffd1ef826ff4ee79ec
simongoode <simongoode@brandeis.edu>
2024-10-16 21:08:30 -0400

    citing articles, paragraph

--- README.md
+++ README.md
@@ -5,6 +5,13 @@ Highlighting trends in the Rust Belt and the effect of recent industrial policy 
 [The revolt of the Rust Belt: place and politics in the age of anger](https://onlinelibrary.wiley.com/doi/full/10.1111/1468-4446.12328#bjos12328-note-0002_3)  
 [Labor Market Conflict and the Decline of the Rust Belt](https://www.journals.uchicago.edu/doi/full/10.1086/724852)
 
+## articles cited
+[Trump, a Symptom of the Diseased American Democracy](https://www.institutmontaigne.org/en/expressions/trump-symptom-diseased-american-democracy)  
+[The Problematic Symptom of Donald J. Trump](https://smallwarsjournal.com/jrnl/art/problematic-symptom-donald-j-trump)  
+[Trump is the Symptom, not the Problem](https://www.carnegiecouncil.org/media/article/trump-is-the-symptom-not-the-problem)  
+[Is Trump a symptom or the cause of the US's bitter political divide?](https://www.thetimes.com/world/us-world/article/is-trump-a-symptom-or-the-cause-of-the-us-s-bitterpolitical-divide-ghrb9pz2b)  
+[Trump is a Symptom of our moral decline, not the cause](https://www.bostonglobe.com/2021/01/10/opinion/trump-is-symptom-our-moral-decline-not-cause/)  
+
 ## data sources
 [MIT Election Lab](https://electionlab.mit.edu/data)  
 [St. Louis FRED](https://fred.stlouisfed.org/)  
--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import pandas as pd
 from scripts.parse_chart1 import *
 
-st.set_page_config(page_title='Made in America', layout="wide")
+st.set_page_config(page_title='WTF Happened in 2016?', layout="wide")
 
 def local_css(file_name):
     with open(file_name) as f:
@@ -17,13 +17,13 @@ with col1:
 
 with col2:
     st.markdown('<div>', unsafe_allow_html=True)
-    tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Overview", "Dismantled", "Donald Trump", "Joe Biden", "The Future", "About"])
+    tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["Overview", "Dismantled", "Michigan", "Ohio", "Pennsylvania", "Wisconsin", "About"])
 
     # overview
     with tab1:
 
         # st.image("src/visuals/rustbelt.png", width=750)
-        st.markdown('## Abandonment - Anger - Opportunity')
+        st.markdown('## Why Here? Why Now?')
 
         # vote margin differential from previous election, by county
         year = st.slider("Year:", 2004, 2020, step=4)
@@ -31,13 +31,27 @@ with col2:
         st.plotly_chart(get_vote_margin_diff(year))
 
         st.markdown("""
-                    This map shows the change in partisan vote differential, by county, since the previous election.
+                    How much do swing counties swing? This map shows how much bluer or redder each county of these Rust Belt states shifted from the prior presidential election.
         """)
+        st.warning('Play around with the slider. What swing do you see for Obama/McCain in 2008? For Obama/Romney? For Trump/Clinton in 2016?')
+        # paragraph 1
+        st.markdown('''Volumes of research<sup>[\[1\]](https://www.institutmontaigne.org/en/expressions/trump-symptom-diseased-american-democracy)
+                    [\[2\]](https://smallwarsjournal.com/jrnl/art/problematic-symptom-donald-j-trump)</sup>and commentary<sup>
+                    [\[3\]](https://www.carnegiecouncil.org/media/article/trump-is-the-symptom-not-the-problem)
+                    [\[4\]](https://www.thetimes.com/world/us-world/article/is-trump-a-symptom-or-the-cause-of-the-us-s-bitterpolitical-divide-ghrb9pz2b)
+                    [\[5\]](https://www.bostonglobe.com/2021/01/10/opinion/trump-is-symptom-our-moral-decline-not-cause/)</sup>
+                    since 2016 point to Donald Trump being a symptom more than the cause of the growing division and recent populist slant in United States politics. 
+                    But if it wasn't Trump riding down the escalator in 2016, that begs the question: why here? And why now?''', unsafe_allow_html=True)
 
         st.markdown('</div>', unsafe_allow_html=True)
 
-    # about
+    #michigan
+    with tab3:
+        st.image("src/visuals/watercolor/michigan.png", width=400)
     with tab6:
+        st.image("src/visuals/watercolor/wisconsin.png", width=400)
+    # about
+    with tab7:
         st.markdown("""
             Hi there, I'm Simon Goode, a student at Brandeis University studying Applied Math and Economics.
             
@@ -56,7 +70,7 @@ with col2:
      <textarea name="message" placeholder="Your message here"></textarea>
      <input type="file" class="img_btn" name="Upload Image" accept="image/png, image/jpeg">
      <br>
-     <input type="hidden" name="_next" value="https://dezoomcamp.streamlit.app/Thank%20you">
+     <input type="hidden" name="_next" value="localhost:8501">
      <button type="submit">Send</button>
 </form>
 """
